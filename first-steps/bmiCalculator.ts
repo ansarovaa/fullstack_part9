e8cf6166ec056c9aefdbb78028a61d07fd9d4cee
Anar Ansarova <Anar_Ansarova@epam.com>
2021-04-14 11:45:32 +0600

    task 9.5

--- first-steps/bmiCalculator.ts
+++ first-steps/bmiCalculator.ts
@@ -1,24 +1,26 @@
-const calculateBmi = (a: number, b: number, printText: string) => {
+export const calculateBmi = (a: number, b: number) : string => {
   const heightInMeter = a / 100
   const bmi = b / (heightInMeter*heightInMeter)
   
   if (bmi < 18.5) {
-    printText = "Underweight";
+    console.log("Underweight");
+    return "Underweight";
   } else if (bmi >= 18.5 && bmi < 23) {
-    printText = "Normal range";
+    console.log("Normal range");
+    return "Normal range";
   } else if (bmi >= 23 && bmi < 25) {
-    printText = "Overweight—At Risk";
+    console.log("Overweight—At Risk");
+    return "Overweight—At Risk";
   } else if (bmi >= 25 && bmi < 30) {
-    printText = "Overweight—Moderately Obese";
+    console.log("Overweight—Moderately Obese");
+    return "Overweight—Moderately Obese";
   } else if (bmi >= 30) {
-    printText = "Overweight—Severely Obese";
+    console.log("Overweight—Severely Obese");
+    return "Overweight—Severely Obese";
   } 
 
-  console.log(printText, bmi)
+  return `Oops something went wrong, unable to calculate bmi`;
+
+
 }
  
-
-
-const a: number = Number(process.argv[2])
-const b: number = Number(process.argv[3])
-calculateBmi(a, b, ``)
--- first-steps/index.ts
+++ first-steps/index.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import { calculateBmi}  from "./bmiCalculator";
 const app = express();
 
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get("/bmi", (req, res) => {
+  const { weight, height } = req.query;
+  const validData: boolean =
+    !isNaN(Number(height)) && !isNaN(Number(weight));
+  const bmi = calculateBmi(Number(height), Number(weight));
+
+  if (!validData || !weight || !height) {
+    res.send({
+      error: "malformatted parameters",
+    });
+  }
+
+  res.send({ weight, height, bmi });
 });
 
 const PORT = 3003;
