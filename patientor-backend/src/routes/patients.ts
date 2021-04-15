84d17506cdef1c3561f13a04441b254baf720b07
Anar Ansarova <Anar_Ansarova@epam.com>
2021-04-15 11:40:44 +0600

    task 9.12 and 9.13

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import express from 'express';
 import patientsService from "../services/patientsService";
+import toNewPatient from "../utils";
 
 const router = express.Router();
 
@@ -7,8 +9,17 @@ router.get('/', (_req, res) => {
   res.send(patientsService.getEntries());
 });
 
-router.post('/', (_req, res) => {
-  res.send('Saving a patient!');
+router.post("/", (req, res) => {
+  try {
+    const newPatient = toNewPatient(req.body);
+
+    const addedPatient = patientsService.addEntry(newPatient);
+
+    res.json(addedPatient);
+  } catch (e) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    res.status(400).send(e.message);
+  }
 });
 
 export default router;
--- patientor-backend/src/services/patientsService.ts
+++ patientor-backend/src/services/patientsService.ts
@@ -1,12 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import patientsData from '../../data/patients.json';
+import { Patient, NewPatient } from "../types";
+const patients: Array<Patient> = patientsData;
 
 const getEntries = () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return patientsData;
 };
 
-const addEntry = () => {
-  return null;
+const addEntry = (patient: NewPatient): Patient => {
+  const newPatient = {
+    id: (patients.length + 1).toString(),
+    ...patient,
+  };
+
+  patients.push(newPatient);
+  return newPatient;
 };
 
 export default {
